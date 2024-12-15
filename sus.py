23f0bbea94de263f352df0ea33e33bd62c701f3f
SAD DUST <90087346+SAD-DUST@users.noreply.github.com>
2024-12-15 22:55:42 +0700

    Update sus.py

--- sus.py
+++ sus.py
@@ -1,130 +1,246 @@
 import asyncio
-import random
-import ssl
+import signal
+import cloudscraper
+import requests
 import json
 import time
 import uuid
-import requests
-import shutil
 from loguru import logger
-from websockets_proxy import Proxy, proxy_connect
-from fake_useragent import UserAgent
+import os
+import random
+import sys
+import threading
 
-async def connect_to_wss(socks5_proxy, user_id):
-    user_agent = UserAgent(os=['windows', 'macos', 'linux'], browsers='chrome')
-    random_user_agent = user_agent.random
-    device_id = str(uuid.uuid3(uuid.NAMESPACE_DNS, socks5_proxy))
-    logger.info(device_id)
-    while True:
-        try:
-            await asyncio.sleep(random.randint(1, 10) / 10)
-            custom_headers = {
-                "User-Agent": random_user_agent,
-            }
-            ssl_context = ssl.create_default_context()
-            ssl_context.check_hostname = False
-            ssl_context.verify_mode = ssl.CERT_NONE
-            urilist = ["wss://proxy2.wynd.network:4444/", "wss://proxy2.wynd.network:4650/"]
-            uri = random.choice(urilist)
-            server_hostname = "proxy2.wynd.network"
-            proxy = Proxy.from_url(socks5_proxy)
-            async with proxy_connect(uri, proxy=proxy, ssl=ssl_context, server_hostname=server_hostname,
-                                     extra_headers=custom_headers) as websocket:
-                async def send_ping():
-                    while True:
-                        send_message = json.dumps({
-                            "id": str(uuid.uuid4()), "version": "1.0.0", "action": "PING", "data": {}
-                        })
-                        logger.debug(send_message)
-                        await websocket.send(send_message)
-                        await asyncio.sleep(5)
+# Determine the directory where the script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-                await asyncio.sleep(1)
-                asyncio.create_task(send_ping())
+# Function to read a single-line token from a file
+def read_single_line_file(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            return f.read().strip()
+    return None
 
-                while True:
-                    response = await websocket.recv()
-                    message = json.loads(response)
-                    logger.info(message)
-                    if message.get("action") == "AUTH":
-                        auth_response = {
-                            "id": message["id"],
-                            "origin_action": "AUTH",
-                            "result": {
-                                "browser_id": device_id,
-                                "user_id": user_id,
-                                "user_agent": custom_headers['User-Agent'],
-                                "timestamp": int(time.time()),
-                                "device_type": "desktop",
-                                "version": "4.30.0",
-                            }
-                        }
-                        logger.debug(auth_response)
-                        await websocket.send(json.dumps(auth_response))
+# Function to read multiple lines from a file
+def read_lines_file(file_path):
+    with open(file_path, 'r') as f:
+        return f.read().splitlines()
 
-                    elif message.get("action") == "PONG":
-                        pong_response = {"id": message["id"], "origin_action": "PONG"}
-                        logger.debug(pong_response)
-                        await websocket.send(json.dumps(pong_response))
-        except Exception as e:
-            proxy_to_remove = socks5_proxy
-            with open('auto_proxies.txt', 'r') as file:
-                lines = file.readlines()
-            updated_lines = [line for line in lines if line.strip() != proxy_to_remove]
-            with open('auto_proxies.txt', 'w') as file:
-                file.writelines(updated_lines)
-            #print(f"Proxy '{proxy_to_remove}' has been removed from the file.")
+# Function to filter out empty lines
+def filter_non_empty_lines(lines):
+    return [line for line in lines if line.strip()]
 
-def fetch_proxies():
-    """Fetches proxies from the API and saves them to 'auto_proxies.txt'."""
-    api_url = "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=protocolipport&format=text"
+# Read configuration values from files
+NP_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzEwMjQ3ODQ1MzE5NjcxODA4IiwiaWF0IjoxNzMzODM1NTQwLCJleHAiOjE3MzUwNDUxNDB9.JSI41mtK9aaf8MKNPM5oR0DSJHDjJNizoifgiVbGiTeb3_xjKzUFo_WSV8_r1293TLGr0WFB4GBbdoXtSEGe_A"
+
+r = requests.get("https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text", stream=True)
+if r.status_code == 200:
+    with open('proxies.txt', 'wb') as f:
+        for chunk in r:
+            f.write(chunk)
+    with open('proxies.txt', 'r') as file:
+        auto_proxy_list = file.read().splitlines()
+
+all_proxies = filter_non_empty_lines(read_lines_file(os.path.join(script_dir, 'proxies.txt')))
+proxy_type = read_single_line_file(os.path.join(script_dir, 'proxy-config.txt')) or 'http'
+
+# Constants
+PING_URL = "https://nw2.nodepay.org/api/network/ping"
+AUTH_URL = "http://api.nodepay.ai/api/auth/session"
+RETRY_INTERVAL = 60  # Retry interval for failed proxies in seconds
+EXTENSION_VERSION = "2.2.7"
+GITHUB_REPO = "NodeFarmer/nodepay"
+CURRENT_VERSION = "1.4.0"
+NODEPY_FILENAME = "nodepay.py"
+
+# Function to download the latest version of the script
+def download_latest_version():
+    url = f"https://raw.githubusercontent.com/{GITHUB_REPO}/main/{NODEPY_FILENAME}"
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(os.path.join(script_dir, NODEPY_FILENAME), 'wb') as f:
+        f.write(response.content)
+
+# Function to check for updates and download if available
+def check_for_update():
     try:
-        response = requests.get(api_url, stream=True)
-        if response.status_code == 200:
-            proxies = response.text.strip().splitlines()
-            if proxies:
-                with open('auto_proxies.txt', 'w') as f:
-                    f.writelines([proxy + '\n' for proxy in proxies])
-                print(f"Fetched and saved {len(proxies)} proxies to 'auto_proxies.txt'.")
-            else:
-                print("No proxies found from the API.")
-                return False
+        url = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
+        response = requests.get(url)
+        response.raise_for_status()
+        latest_release = response.json()
+        latest_version = latest_release["tag_name"]
+
+        if latest_version != CURRENT_VERSION:
+            logger.info(f"New version available: {latest_version}. Current version: {CURRENT_VERSION}")
+            download_latest_version()
+            logger.info("Downloaded latest version of nodepay.py")
+            return True
         else:
-            print(f"Failed to fetch proxies. Status code: {response.status_code}")
+            logger.info("No new version available.")
             return False
     except Exception as e:
-        print(f"Error fetching proxies: {e}")
+        logger.error(f"Error checking for update: {e}")
         return False
-    return True
 
+# Function to restart the script
+def restart_script():
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+
+def format_proxy(proxy_string, proxy_type):
+    prefix = ''
+    if proxy_type in ['http', 'https']:
+        prefix = 'http://'
+    elif proxy_type in ['socks', 'socks5']:
+        prefix = 'socks5://'
+    else:
+        raise ValueError("Invalid proxy type")
+    
+    if '@' in proxy_string:
+        # Format: user:pwd@ip:port
+        user_pwd, ip_port = proxy_string.split('@')
+        user, pwd = user_pwd.split(':')
+        ip, port = ip_port.split(':')
+        formatted_proxy = f"{prefix}{user}:{pwd}@{ip}:{port}"
+    elif proxy_string.count(':') == 3:
+        # Format: IP:PORT:USERNAME:PASSWORD
+        ip, port, user, pwd = proxy_string.split(':')
+        formatted_proxy = f"{prefix}{user}:{pwd}@{ip}:{port}"
+    elif proxy_string.count(':') == 1:
+        # Format: IP:PORT (No user or password)
+        ip, port = proxy_string.split(':')
+        formatted_proxy = f"{prefix}{ip}:{port}"
+    else:
+        raise ValueError("Invalid proxy string format")
+    
+    return formatted_proxy
+
+def uuidv4():
+    template = "10000000-1000-4000-8000-100000000000"
+    return ''.join([
+        hex(random.randint(0, 15) ^ (random.getrandbits(8) & (15 >> (int(c) // 4))))[2:]
+        if c in '018' else c
+        for c in template
+    ])
+
+def send_ping(proxy_url, user_id, token):
+    logger.info(f"Starting thread for proxy: {proxy_url}")
+    browser_id = uuidv4()
+    scraper = cloudscraper.create_scraper(
+        browser={
+            'browser': 'chrome',
+            'platform': 'windows',
+            'desktop': True
+        }
+    )
+    while True:
+        try:
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Referer": "https://app.nodepay.ai/",
+                "Accept": "application/json, text/plain, */*",
+                "Content-Type": "application/json",
+                "Origin": "chrome-extension://lgmpfmgeabnnlemejacfljbmonaomfmm",
+            }
+            headers['Authorization'] = f'Bearer {token}'
+            payload = {
+                "user_id": user_id,
+                "browser_id": browser_id,
+                "timestamp": int(time.time()),
+                "version": EXTENSION_VERSION
+            }
+            proxies = {
+                'http': proxy_url,
+                'https': proxy_url
+            }
+            response = scraper.post(PING_URL, headers=headers, json=payload, proxies=proxies)
+            response.raise_for_status()
+            logger.debug(response.json())
+            time.sleep(10)  # Wait for a while before the next action
+        except Exception as e:
+            logger.error(f"Error with proxy {proxy_url}: {e}")
+            time.sleep(RETRY_INTERVAL)
+
+async def call_api_info(token, proxy_url):
+    logger.info("Getting UserID")
+    scraper = cloudscraper.create_scraper(
+    browser={
+        'browser': 'chrome',
+        'platform': 'windows',
+        'desktop': True
+    }
+)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": "https://app.nodepay.ai/",
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+        "Origin": "chrome-extension://lgmpfmgeabnnlemejacfljbmonaomfmm",
+    }
+    if token:
+        headers['Authorization'] = f'Bearer {token}'
+    
+    proxies = {
+        'http': proxy_url,
+        'https': proxy_url
+    }
+    
+    response = scraper.post(
+        AUTH_URL,
+        headers=headers,
+        json={},
+        proxies=proxies
+    )
+    response.raise_for_status()
+    return response.json()
+# Main function to run the program
+# Updated `call_api_info` call to await it properly
 async def main():
-    try:
-        _user_id = "2p4GgmhEwvn4B8NPpwyxHnAbzjk"
-        if not _user_id:
-            return
-        print(f"User ID read from file: {_user_id}")
-    except FileNotFoundError:
-        print("Error: 'user_id.txt' file not found.")
-        return
+    if check_for_update():
+        logger.info("Restarting script to apply new version...")
+        restart_script()
+    
+    max_attempts = 10
+    attempt = 0
+    user_data = None
 
-    # Fetch and save proxies to 'auto_proxies.txt'
-    if not fetch_proxies():
-        print("No proxies available. Exiting script.")
-        return
+    while attempt < max_attempts and user_data is None:
+        attempt += 1
+        logger.info(f"Authentication attempt {attempt} of {max_attempts}")
+        if NP_TOKEN != "":
+            for proxy_string in all_proxies:
+                first_proxy = format_proxy(proxy_string, proxy_type)
+                try:
+                    user_data = await call_api_info(NP_TOKEN, first_proxy) 
+                    break
+                except Exception as e:
+                    logger.error(f"Proxy {first_proxy} is invalid: {e}")
+                    continue
+        else:
+            logger.error("You need to specify NP_TOKEN value inside token.txt")
+            break
 
-    try:
-        with open('auto_proxies.txt', 'r') as file:
-            auto_proxy_list = file.read().splitlines()
-            if not auto_proxy_list:
-                print("No proxies found in 'auto_proxies.txt'. Exiting script.")
-                return
-            print(f"Proxies read from file: {auto_proxy_list}")
-    except FileNotFoundError:
-        print("Error: 'auto_proxies.txt' file not found.")
-        return
+        if user_data is None:
+            logger.warning("Authentication failed. Retrying...")
+            await asyncio.sleep(1)  # Use asyncio.sleep for async context
 
-    tasks = [asyncio.ensure_future(connect_to_wss(i, _user_id)) for i in auto_proxy_list]
-    await asyncio.gather(*tasks)
+    if user_data:
+        logger.debug(user_data)
+        USER_ID = user_data['data']['uid']
+        threads = []
+        for proxy_string in all_proxies:
+            proxy = format_proxy(proxy_string, proxy_type)
+            thread = threading.Thread(target=send_ping, args=(proxy, USER_ID, NP_TOKEN))
+            threads.append(thread)
+            thread.start()
 
+        for thread in threads:
+            thread.join()
+    else:
+        logger.error("Authentication failed after 10 attempts. Exiting.")
+
+# Use asyncio.run to start the async main function
 if __name__ == '__main__':
     asyncio.run(main())
